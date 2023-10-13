811824dd94fd74ec57599db14ea55f6fcaad9530
kishan <kishans@webmobtech.com>
2023-10-13 15:07:40 +0530

    done third  program

---
+++ Kishan Sakhiya/ques3.dart
@@ -0,0 +1,28 @@
+const List<int> listIntNumber = [
+  50, 40, 10, 9, 5, 4, 1
+];
+
+const List<String> romanNumbers = [
+  "L", "XL", "X", "IX", "V", "IV", "I"
+];
+
+String intToRoman(int input) {
+  var num = input;
+
+  final builders = StringBuffer();
+  for (var a = 0; a < listIntNumber.length; a++) {
+
+    final times = (num / listIntNumber[a]).truncate();
+    builders.write(romanNumbers[a] * times);
+    num -= times * listIntNumber[a];
+  }
+
+  return builders.toString();
+}
+
+void main() {
+  for (var i = 1; i <= 50; i++) {
+    print('$i : ${intToRoman(i)}');
+  }
+
+}
