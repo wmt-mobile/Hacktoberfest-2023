00950eba31265dd6942b81dc3643259b179edc7f
deept <deept@webmobtech.com>
2023-10-13 13:34:35 +0530

    q3

---
+++ Deep Thumar/question_3.js
@@ -0,0 +1,27 @@
+function isValid(s) {
+  const stack = [];
+  const bracketsMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const currentChar = s[i];
+
+    if (bracketsMap[currentChar]) {
+      stack.push(currentChar);
+    } else {
+      const lastBracket = stack.pop();
+
+      if (bracketsMap[lastBracket] !== currentChar) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("()"));
+console.log(isValid("(]"));
