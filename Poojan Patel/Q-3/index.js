856a86e6db7f9ac0095aeb230cd54d0b0f747334
Jeet Singh <jeets@webmobtech.com>
2023-10-13 14:48:06 +0530

    2nd sol

---
+++ Poojan Patel/Q-3/index.js
@@ -0,0 +1,13 @@
+function convertToZeros(arr) {
+  const sum = arr.reduce((acc, num) => acc + num, 0);
+
+  if (sum >= 0) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
+
+const arr = [1, 2, 3];
+const result = convertToZeros(arr);
+console.log(result);
